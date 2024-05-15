cadd1c2517cefdf78995be55fe8044868ec30109
Nat <usolnatalia@gmail.com>
2024-05-15 20:34:07 +1200

    Restructure the layout for a goal with/out transactions

--- src/pages/SavingGoalsPage/components/Goal.tsx
+++ src/pages/SavingGoalsPage/components/Goal.tsx
@@ -18,53 +18,70 @@ const Goal = ({ goal, onDelete, onEdit, onAdd }: Props) => {
     const [editVisible, setEditVisible] = useState(false);
     const [transactionVisible, setTransactionVisible] = useState(false);
     const [isTransactionsExpanded, setIsTransactionsExpanded] = useState(false);
+    const [isGoalExpanded, setIsGoalExpanded] = useState(false);
 
     const onTransactionToggle = () => {
         setIsTransactionsExpanded((prevState: boolean) => !prevState);
+    }
+
+    const onGoalToggle = () => {
+        setIsGoalExpanded((prevState: boolean) => !prevState);
     }
 
     const seconds = getDateDiffSeconds(goal.date);
 
     return (
         <View style={styles.container}>
-            <View style={styles.itemContainer}>
-                <Text>Name: {goal.name}</Text>
-                <Text>Amount: {goal.amount.toString()}</Text>
-                <Text>SavedAmount: {goal.savedAmount.toString()}</Text>
-                <Text>Date: {formatDate(goal.date)}</Text>
-                {goal.transactions.length > 0 && (
-                    <View style={styles.transactionsContainer}>
-                        <Button accessoryLeft={<Icon name={'arrow-ios-downward'} />} onPress={onTransactionToggle} appearance='ghost' style={buttonStyle(isTransactionsExpanded).icon} />
-                        {isTransactionsExpanded && goal.transactions.map((item: TransactionItem, i: number) => (
-                            <TransactionItem key={i} item={item} />
-                        ))}
+            <View style={styles.goalContainer}>
+                <View style={{ flexDirection: "row", alignItems: "center" }}>
+                    <Button accessoryLeft={<Icon name={'arrow-ios-downward'} />} onPress={onGoalToggle} appearance='ghost' style={buttonStyle(isGoalExpanded).icon} />
+                    <Text>Name: {goal.name}</Text>
+                </View>
+                <View style={styles.actionContainer}>
+                    <Button accessoryLeft={<Icon name='trash' />} onPress={onDelete} appearance='ghost' status='danger' style={styles.button} />
+                    <Button accessoryLeft={<Icon name='edit' />} onPress={() => setEditVisible(true)} appearance='ghost' status='primary' style={styles.button} />
+                </View>
+            </View>
+            {isGoalExpanded && (
+                <View style={{ alignItems: "flex-start", marginLeft: 35 }}>
+                    <Text>Amount: {goal.amount.toString()}</Text>
+                    <Text>SavedAmount: {goal.savedAmount.toString()}</Text>
+                    <Text>Date: {formatDate(goal.date)}</Text>
+                    <CountDown
+                        until={seconds || 0}
+                        onFinish={() => console.log('finished')}
+                        size={20}
+                        timeLabelStyle={styles.timeLabel}
+                        digitStyle={styles.time}
+                    />
+                    <View style={{ flexDirection: "row", alignItems: "center" }}>
+                        <Text>Transactions:</Text>
+                        <Button accessoryLeft={<Icon name='plus' />} onPress={() => setTransactionVisible(true)} appearance='ghost' status='primary' style={styles.button} />
                     </View>
-                )}
-            </View>
-            <CircularProgressBar progress={goal.savedAmount / goal.amount} />
-            <CountDown
-                until={seconds || 0}
-                onFinish={() => console.log('finished')}
-                size={20}
-                timeLabelStyle={styles.timeLabel}
-                digitStyle={styles.time}
-            />
-            <View style={styles.actionContainer}>
-                <Button accessoryLeft={<Icon name='trash' />} onPress={onDelete} appearance='ghost' status='danger' style={styles.button} />
-                <Button accessoryLeft={<Icon name='edit' />} onPress={() => setEditVisible(true)} appearance='ghost' status='primary' style={styles.button} />
-                <Button accessoryLeft={<Icon name='plus' />} onPress={() => setTransactionVisible(true)} appearance='ghost' status='primary' style={styles.button} />
-            </View>
-            <GoalModal
-                onSave={onEdit}
-                isVisible={editVisible}
-                onClose={() => setEditVisible(false)}
-                goal={goal}
-            />
-            <TransactionModal
-                onSave={onAdd}
-                isVisible={transactionVisible}
-                onClose={() => setTransactionVisible(false)}
-            />
+                    {goal.transactions.length > 0 && (
+                        <View style={styles.transactionsContainer}>
+                            {goal.transactions.map((item: TransactionItem, i: number) => (
+                                <TransactionItem key={i} item={item} />
+                            ))}
+                        </View>
+                    )}
+                </View>
+            )}
+            {editVisible && (
+                <GoalModal
+                    onSave={onEdit}
+                    isVisible={true}
+                    onClose={() => setEditVisible(false)}
+                    goal={goal}
+                />
+            )}
+            {transactionVisible && (
+                <TransactionModal
+                    onSave={onAdd}
+                    isVisible={true}
+                    onClose={() => setTransactionVisible(false)}
+                />
+            )}
         </View>
     );
 }
@@ -73,10 +90,14 @@ export default Goal;
 
 const styles = StyleSheet.create({
     container: {
-        flexDirection: "row",
+        flexDirection: "column",
+        marginBottom: 20,
     },
-    itemContainer: {
-        marginBottom: 20
+    goalContainer: {
+        flexDirection: 'row',
+        justifyContent: "space-between",
+        alignItems: "center",
+        height: 30,
     },
     button: {
         width: 20
@@ -91,7 +112,7 @@ const styles = StyleSheet.create({
         backgroundColor: "#a3c2e3",
     },
     actionContainer: {
-        flexDirection: "column"
+        flexDirection: "row"
     },
     transactionsContainer: {
         marginLeft: 20
