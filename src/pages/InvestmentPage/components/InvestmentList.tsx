5400e2e72a3e077253aea0eba4c8063da8e3a2d2
Nat <usolnatalia@gmail.com>
2024-05-31 21:38:30 +1200

    Remove unused variables and imports

--- src/pages/InvestmentPage/components/InvestmentList.tsx
+++ src/pages/InvestmentPage/components/InvestmentList.tsx
@@ -1,6 +1,5 @@
-import { List, ListItem } from "@ui-kitten/components";
-import { useState } from "react";
-import { StyleSheet, View } from "react-native";
+import { ListItem } from "@ui-kitten/components";
+import { View } from "react-native";
 import useTable from "../hooks/useTable";
 
 const HEADERS = ["Year", "Contribution", "Returns", "Total"];
@@ -10,8 +9,6 @@ type Props = {
 }
 
 const InvestmentList = ({ item }: Props) => {
-    const [random, setRandom] = useState(Math.random());
-
     const { data } = useTable({ item });
     return (
         new Array(data.length + 1).fill(0).map((_, index) => (
@@ -31,27 +28,3 @@ const InvestmentList = ({ item }: Props) => {
 }
 
 export default InvestmentList;
-
-const styles = StyleSheet.create({
-    incomeView: {
-        display: "flex",
-        flexDirection: 'row',
-        position: "relative"
-    },
-    container: {
-        minWidth: 150
-    },
-    input: {
-        flexGrow: 1
-    },
-    dollarSign: {
-        position: "absolute",
-        zIndex: 5,
-        color: "red",
-        top: "28%",
-        left: "2%",
-    },
-    row: {
-        color: "red",
-    }
-});
