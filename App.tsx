b12c8e268fe50542640d6cb6931d218eb8e3a5d0
nataliausoltseva <usolnatalia@gmail.com>
2024-07-28 18:16:55 +1200

    Make all inputs available when keyboard is open

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { SafeAreaView, ScrollView, View } from 'react-native';
+import { KeyboardAvoidingView, SafeAreaView, ScrollView, View } from 'react-native';
 import * as eva from '@eva-design/eva';
 import { ApplicationProvider } from '@ui-kitten/components';
 import AsyncStorage from '@react-native-async-storage/async-storage';
@@ -95,57 +95,51 @@ function App(): React.JSX.Element {
     }
   }
 
-  const onSaveHistory = () => {
-    if (TABS[selectedIndex].key === 'income') {
-      getIncomeData();
-    } else {
-      getBudgetData();
-    }
-  }
-
   const hasHistoryButton = (TABS[selectedIndex].key === 'income' && incomeData.length > 0) ||
     (TABS[selectedIndex].key === 'budget' && budgetData.length > 0);
 
   return (
     <SafeAreaView>
-      <ApplicationProvider {...eva} theme={isDarkMode ? eva.dark : eva.light}>
-        <AppContextProvider>
-          <SafeAreaView style={{ backgroundColor: isDarkMode ? '#443472' : "white", height: "100%" }}>
-            <TopNavigationBar tabs={TABS} onSelect={setSelectedIndex} selectedIndex={selectedIndex} />
-            <ScrollView style={{ margin: 20, flexGrow: 1 }}>
-              <IncomePage
-                isHidden={TABS[selectedIndex].key !== 'income'}
-                showHistoryModal={showHistoryModal === 'income'}
-                storageData={incomeData}
-                onCloseModal={() => setShowHistoryModal('')}
-                onDeleteStorageItem={onDeleteStorageItem}
-                onSaveHistory={getIncomeData}
-              />
-              <BudgetPage
-                isHidden={TABS[selectedIndex].key !== 'budget'}
-                showHistoryModal={showHistoryModal === 'budget'}
-                storageData={budgetData}
-                onCloseHistoryModal={() => setShowHistoryModal('')}
-                onDeleteStorageItem={onDeleteStorageItem}
-                onSaveHistory={getBudgetData}
-              />
-              <SavingGoalsPage isHidden={TABS[selectedIndex].key !== 'savingGoals'} />
-              <InvestmentPage isHidden={TABS[selectedIndex].key !== 'investment'} />
-            </ScrollView>
-            <View style={{ flexDirection: 'row', width: "100%", justifyContent: hasHistoryButton ? "space-between" : 'flex-end', alignItems: "center" }}>
-              {hasHistoryButton && (
-                <Button
-                  containerStyle={{ flexDirection: "row", padding: 8, marginLeft: 10, borderRadius: 2 }}
-                  onPress={() => setShowHistoryModal(TABS[selectedIndex].key)}
-                  Icon={HistoryIcon}
-                  label='OLD CALCULATIONS'
+      <KeyboardAvoidingView>
+        <ApplicationProvider {...eva} theme={isDarkMode ? eva.dark : eva.light}>
+          <AppContextProvider>
+            <SafeAreaView style={{ backgroundColor: isDarkMode ? '#443472' : "white", height: "100%" }}>
+              <TopNavigationBar tabs={TABS} onSelect={setSelectedIndex} selectedIndex={selectedIndex} />
+              <ScrollView style={{ margin: 20, flexGrow: 1 }}>
+                <IncomePage
+                  isHidden={TABS[selectedIndex].key !== 'income'}
+                  showHistoryModal={showHistoryModal === 'income'}
+                  storageData={incomeData}
+                  onCloseModal={() => setShowHistoryModal('')}
+                  onDeleteStorageItem={onDeleteStorageItem}
+                  onSaveHistory={getIncomeData}
                 />
-              )}
-              <DarkModeToggle onToggle={onModeChange} />
-            </View>
-          </SafeAreaView>
-        </AppContextProvider>
-      </ApplicationProvider>
+                <BudgetPage
+                  isHidden={TABS[selectedIndex].key !== 'budget'}
+                  showHistoryModal={showHistoryModal === 'budget'}
+                  storageData={budgetData}
+                  onCloseHistoryModal={() => setShowHistoryModal('')}
+                  onDeleteStorageItem={onDeleteStorageItem}
+                  onSaveHistory={getBudgetData}
+                />
+                <SavingGoalsPage isHidden={TABS[selectedIndex].key !== 'savingGoals'} />
+                <InvestmentPage isHidden={TABS[selectedIndex].key !== 'investment'} />
+              </ScrollView>
+              <View style={{ flexDirection: 'row', width: "100%", justifyContent: hasHistoryButton ? "space-between" : 'flex-end', alignItems: "center" }}>
+                {hasHistoryButton && (
+                  <Button
+                    containerStyle={{ flexDirection: "row", padding: 8, marginLeft: 10, borderRadius: 2 }}
+                    onPress={() => setShowHistoryModal(TABS[selectedIndex].key)}
+                    Icon={HistoryIcon}
+                    label='OLD CALCULATIONS'
+                  />
+                )}
+                <DarkModeToggle onToggle={onModeChange} />
+              </View>
+            </SafeAreaView>
+          </AppContextProvider>
+        </ApplicationProvider>
+      </KeyboardAvoidingView>
     </SafeAreaView >
   );
 }
